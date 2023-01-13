fbe6226c5a1c9d75901783a554df2dc1bc987543
meti <mahdikhodarahimi0@gmail.com>
2023-01-13 14:28:20 +0330

    Add project files.

---
+++ EmojiList/Models/Emoji.cs
@@ -0,0 +1,10 @@
+﻿namespace EmojiList.Models
+{
+    public class Emoji
+    {
+        public string Title { get; set; }
+        public string Symbol { get; set; }
+        public string Keywords { get; set; }
+
+    }
+}
---
+++ EmojiList/Program.cs
@@ -0,0 +1,12 @@
+using EmojiList;
+using MatBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMatBlazor();
+await builder.Build().RunAsync();
